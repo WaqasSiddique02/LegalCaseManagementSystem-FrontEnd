88976ca2866522bfd261dd31600a6a635092332b
el-noir <mudasirshah9777@gmail.com>
2025-05-21 04:34:40 +0500

    Second Resolve

--- LegalCaseManagementSystem-FrontEnd/Models/CaseTask.cs
+++ LegalCaseManagementSystem-FrontEnd/Models/CaseTask.cs
@@ -4,11 +4,7 @@ using System.Text.Json.Serialization;
 namespace LegalCaseManagementSystem_FrontEnd.Models
 {
     public class CaseTask
-<<<<<<< HEAD
-    {
-=======
 {
->>>>>>> bff96e2785d853b5d56cfae98c8fb8607b07d282
         [Key]
         public int TaskId { get; set; }
         public int CaseId { get; set; }
@@ -18,13 +14,9 @@ namespace LegalCaseManagementSystem_FrontEnd.Models
 
         public string Status { get; set; } = "Pending";
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? CompletedAt { get; set; }
 
         public Case Case { get; set; } = null!;
         public Lawyer? AssignedToLawyer { get; set; }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> bff96e2785d853b5d56cfae98c8fb8607b07d282
+
